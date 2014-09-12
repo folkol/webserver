b0265784ad7a14121cced90d7faf9db5131391fc
Mattias Johansson <mattias.johansson@atex.com>
2014-09-12 15:16:58 +0200

    Moved the main method to the bottom of the Class

--- src/Main.java
+++ src/Main.java
@@ -15,20 +15,10 @@ public class Main
     public static final byte[] STATUS_NOT_OK = "HTTP/1.1 404 Not Found\r\n\r\n".getBytes();
     public static final byte[] STATUS_OK = "HTTP/1.1 200 OK\r\n\r\n".getBytes();
 
-    public static void main(String[] arguments) throws Exception {
-        ExecutorService workers = Executors.newCachedThreadPool();
-
-        ServerSocket socket = new ServerSocket(8080);
-        while (socket.isBound()) {
-            Socket client = socket.accept();
-            workers.execute(() -> handleConnection(client));
-        }
-    }
-
     private static void handleConnection(Socket client) {
-        try (OutputStream out = client.getOutputStream()) {
+        try (OutputStream out = client.getOutputStream())
+        {
             Scanner in = new Scanner(client.getInputStream());
-
             String method = in.next();
             String resource = in.next();
 
@@ -41,6 +31,16 @@ public class Main
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] arguments) throws Exception {
+        ServerSocket socket = new ServerSocket(8080);
+
+        ExecutorService workers = Executors.newCachedThreadPool();
+        while (socket.isBound()) {
+            Socket client = socket.accept();
+            workers.execute(() -> handleConnection(client));
         }
     }
 
