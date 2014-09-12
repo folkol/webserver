67666805bf32d7bf4d39f533f8625ab3ef8a6875
Mattias Johansson <mattias.johansson@atex.com>
2014-09-12 10:14:30 +0200

    Increase project language level to 8, and added a naïve main method

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.OutputStream;
+import java.net.ServerSocket;
+
+public class Main
+{
+    public static void main(String[] arguments) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        try (OutputStream out = serverSocket.accept().getOutputStream()) {
+            out.write("Hello, world!\n".getBytes());
+            out.flush();
+        }
+    }
+}
