31043d3a495ce09e05ac19a66886f7e81c18d4f7
Mattias Johansson <mattias.johansson@atex.com>
2014-09-12 14:59:30 +0200

    Added support for several concurrent users

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,47 @@
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main
 {
+
+    public static final byte[] STATUS_NOT_OK = "HTTP/1.1 404 Not Found\r\n\r\n".getBytes();
+    public static final byte[] STATUS_OK = "HTTP/1.1 200 OK\r\n\r\n".getBytes();
+
     public static void main(String[] arguments) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(8080);
-        try (OutputStream out = serverSocket.accept().getOutputStream()) {
-            out.write("Hello, world!\n".getBytes());
+        ExecutorService workers = Executors.newCachedThreadPool();
+
+        ServerSocket socket = new ServerSocket(8080);
+        while (socket.isBound()) {
+            Socket client = socket.accept();
+            workers.execute(() -> handleConnection(client));
         }
     }
+
+    private static void handleConnection(Socket client) {
+        try (OutputStream out = client.getOutputStream()) {
+            Scanner in = new Scanner(client.getInputStream());
+
+            String method = in.next();
+            String resource = in.next();
+
+            Path file = Paths.get("/Library/WebServer/Documents", resource);
+            if (Files.isRegularFile(file)) {
+                out.write(STATUS_OK);
+                Files.copy(file, out);
+            } else {
+                out.write(STATUS_NOT_OK);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
